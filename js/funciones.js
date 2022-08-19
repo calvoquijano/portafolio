0e68d9b940707ef10aacb222ea1f5233c90c8b31
calvoquijano <calvo@calvoquijano.com>
2022-08-19 02:31:54 -0500

    Parallax

--- js/funciones.js
+++ js/funciones.js
@@ -7,3 +7,25 @@ function menuSuperior() {
     menuSuperior.className = "ocultar__menu-mobile";
   }
 }
+//Scroll
+// window.addEventListener('scroll', () =>{
+//   let imagenPerfil = document.querySelector('.miperfil');
+//   let posicionImagen = imagenPerfil.getBoundingClientRect().top;
+//   let pantallaPosicion = window.innerHeight;
+//   if (posicionImagen < pantallaPosicion){
+//     imagenPerfil.classList.add('aparecer');
+//   }
+//   else if (posicionImagen > pantallaPosicion){
+//     imagenPerfil.classList.remove('aparecer');
+//   }
+// })
+
+//Parallax
+window.addEventListener('scroll', function(e){
+  let imagenPerfil = document.querySelector('.miperfil');
+  let posicionImagen = window.pageYOffset;
+  let pantallaPosicion = posicionImagen * 0.5;
+  // imagenPerfil.style.background = '#000'
+  // console.log(imagenPerfil.style)
+  imagenPerfil.style.transform = 'translate3d('+pantallaPosicion+'px, 0px, 0px)';
+});
