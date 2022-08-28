88c6e658815e75376c34c8e80daf7760422af0ef
calvoquijano <familiaqc@MacBook-Air-de-Andrea.local>
2022-08-27 23:29:27 -0500

    La cinta roja y el header lo hice dinámico para cambiarlos desde un solo js

--- js/funciones.js
+++ js/funciones.js
@@ -79,6 +79,48 @@ window.addEventListener('scroll', function(e){
 });
 
 
+//Menú
+let datosMenu = {
+  logoDesktopURL: "images/logotipo_calvo_quijano.svg",
+  logoMobileURL: "images/logo_calvo_quijano.svg",
+  icoHamburguesa: "images/icos/charm_menu-hamburger.svg",
+  indexURL: "index.html",
+};
+const cambiarMenu = document.querySelector('header');
+cambiarMenu.innerHTML += `
+<nav class="row align-items-center p-2">
+    <h1 class="col-2 col-md-3 logotipo">
+        <a href="index.html" title="Calvo Quijano">
+            <img class="logo__desktop" src="${datosMenu.logoDesktopURL}" alt="Calvo Quijano">
+            <img class="logo__mobile" src="${datosMenu.logoMobileURL}" alt="Calvo Quijano">
+        </a>
+    </h1>
+    <div class="col-2 col-md-6 order-1 order-md-0">
+        <div class="nav text-center menu__mobile">
+            <a href="javascript:void(0);" onclick="menuSuperior()"><img src="${datosMenu.icoHamburguesa}" alt="Menú"></a>
+            <ul id="menuOculto" class="ocultar__menu-mobile">
+                <li class="nav-item"><a class="nav-link" href="${datosMenu.indexURL}" onclick="menuSuperior()">Home</a></li>
+                <li class="nav-item" onclick="menuSuperior()"><a class="nav-link" href="#trabajo">Mi Trabajo</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contacto" onclick="menuSuperior()">Contáctame</a></li>
+            </ul>
+        </div>
+        <ul class="nav justify-content-center menu__desktop">
+            <li class="nav-item"><a class="nav-link" href="${datosMenu.indexURL}">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#trabajo">Mi Trabajo</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contacto">Contáctame</a></li>
+        </ul>
+    </div>
+    <ul class="col-8 col-md-3 nav justify-content-end order-0 order-md-1">
+        <li class="nav-item"><a class="nav-link" href="https://www.linkedin.com/in/andrés-quijano-aguirre-7682854a/" target="_blank"><img src="images/icos/linkedin.svg" alt="LinkedIn Calvo Quijano"></a></li>
+        <li class="nav-item"><a class="nav-link" href="https://www.behance.net/calvoquijano" target="_blank"><img src="images/icos/behance.svg" alt="Behance Calvo Quijano"></a></li>
+    </ul>
+</nav>
+`
+const cintaRoja = document.querySelector('.cinta_roja h2');
+cintaRoja.innerHTML += `Diseñador UX/UI, Diseñador y Desarrollador Web Senior y Animador 2D`
+
+
+
 //Scroll
 // window.addEventListener('scroll', () =>{
 //   let imagenPerfil = document.querySelector('.miperfil');
